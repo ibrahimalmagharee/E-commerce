4b68676d1d10b155640824cafda1a357a43c1749
ibrahimalmagharee <ibrahimalmagree@gmail.com>
2020-08-25 15:25:45 +0300

    =the login of admin and index dashboard is done

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login()
+    {
+        return view('admin.auth.login');
+    }
+
+    public function checkLogin(LoginRequest $request)
+    {
+        $remember_me = $request->has('remember_me') ? true : false;
+        if (auth()->guard('admin')->attempt(['email' => $request->input('email'), 'password' => $request->input('password')], $remember_me)){
+            return redirect()->route('admin.dashboard');
+        }
+
+        return redirect() -> back()->with(['error' => 'هناك خطأ بالبيانات يرجى التحقق']);
+    }
+}
